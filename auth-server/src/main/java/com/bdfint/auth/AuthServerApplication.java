d47a206c6fe56551d6494901e4b747c8665b67fb
fengcheng <lufengcheng@bdfint.com>
2017-08-26 18:04:59 +0800

    init

---
+++ auth-server/src/main/java/com/bdfint/auth/AuthServerApplication.java
@@ -0,0 +1,60 @@
+package com.bdfint.auth;
+
+import org.apache.catalina.filters.RequestDumperFilter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * @author fengcheng
+ * @version 2017/8/24
+ */
+@SpringBootApplication
+@EnableAuthorizationServer
+@RestController
+public class AuthServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthServerApplication.class, args);
+    }
+
+    @Configuration
+    static class MvcConfig extends WebMvcConfigurerAdapter {
+        @Override
+        public void addViewControllers(ViewControllerRegistry registry) {
+            registry.addViewController("login").setViewName("login");
+            registry.addViewController("/").setViewName("index");
+        }
+    }
+
+    @Configuration
+    @Order(-20)
+    static class LoginConfig extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http
+                    .formLogin().loginPage("/login").permitAll()
+                    .and()
+                    .requestMatchers()
+                    .antMatchers("/", "/login", "/oauth/authorize", "/oauth/confirm_access")
+                    .and()
+                    .authorizeRequests()
+                    .anyRequest().authenticated();
+        }
+    }
+
+    @Profile("!cloud")
+    @Bean
+    RequestDumperFilter requestDumperFilter() {
+        return new RequestDumperFilter();
+    }
+}
---
+++ ui/src/main/java/com/bdfint/ui/UiApplication.java
@@ -0,0 +1,80 @@
+package com.bdfint.ui;
+
+import org.apache.catalina.filters.RequestDumperFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.http.RequestEntity;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author fengcheng
+ * @version 2017/8/26
+ */
+@SpringBootApplication
+@EnableOAuth2Sso
+public class UiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UiApplication.class, args);
+    }
+
+    @Controller
+    static class HomeController {
+        @Autowired
+        OAuth2RestTemplate restTemplate;
+        @Value("${messages.url:http://localhost:7777}/api")
+        String messagesUrl;
+
+        @RequestMapping("/")
+        String home(Model model) {
+            List<Message> messages = Arrays.asList(restTemplate.getForObject(messagesUrl + "/messages", Message[].class));
+            model.addAttribute("messages", messages);
+            return "index";
+        }
+
+        @RequestMapping(path = "messages", method = RequestMethod.POST)
+        String postMessages(@RequestParam String text) {
+            Message message = new Message();
+            message.text = text;
+            restTemplate.exchange(RequestEntity
+                    .post(UriComponentsBuilder.fromHttpUrl(messagesUrl).pathSegment("messages").build().toUri())
+                    .body(message), Message.class);
+            return "redirect:/";
+        }
+    }
+
+    public static class Message {
+        public String text;
+        public String username;
+        public LocalDateTime createdAt;
+    }
+
+    @Profile("!cloud")
+    @Bean
+    RequestDumperFilter requestDumperFilter() {
+        return new RequestDumperFilter();
+    }
+
+    @Bean
+    OAuth2RestTemplate oauth2RestTemplate(OAuth2ClientContext oauth2ClientContext, OAuth2ProtectedResourceDetails details) {
+        return new OAuth2RestTemplate(details, oauth2ClientContext);
+    }
+
+}
